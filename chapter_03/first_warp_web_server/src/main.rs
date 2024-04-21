b33990b42c557f4400bccfd542093d7ed46c3a33
Christopher Bartling <chris.bartling@gmail.com>
2024-04-21 08:37:13 -0500

    feat: serializing Question as JSON

--- chapter_03/first_warp_web_server/src/main.rs
+++ chapter_03/first_warp_web_server/src/main.rs
@@ -1,10 +1,85 @@
-use warp::Filter;
+use serde::Serialize;
+use std::fmt::Debug;
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+use warp::{http::StatusCode, reject::Reject, Filter, Rejection, Reply};
+
+#[derive(Debug, Serialize)]
+struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct InvalidId;
+
+impl Reject for InvalidId {}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+
+    match question.id.0.parse::<i32>() {
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+        Ok(_) => Ok(warp::reply::json(&question)),
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(_invalid_id) = r.find::<QuestionId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND,
+        ))
+    }
+}
 
 #[tokio::main]
 async fn main() {
     // create a path Filter
-    let hello = warp::path("hello").map(|| format!("Hello, World!"));
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
 
-    // start the server and pass the route filter to it
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let routes = get_items;
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
