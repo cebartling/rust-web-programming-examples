64ea99fe4e2d3e5c877117fc3036859b97a487f4
Christopher Bartling <chris.bartling@gmail.com>
2024-04-20 21:39:52 -0500

    feat: creating warp HTTP server with filter

---
+++ chapter_03/first_warp_web_server/src/main.rs
@@ -0,0 +1,10 @@
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // create a path Filter
+    let hello = warp::path("hello").map(|| format!("Hello, World!"));
+
+    // start the server and pass the route filter to it
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+}
