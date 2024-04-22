0d930b693af75db64a6408ca200f889256850f75
Christopher Bartling <chris.bartling@gmail.com>
2024-04-21 22:03:52 -0500

    feat: adding CORS

--- chapter_03/first_warp_web_server/src/main.rs
+++ chapter_03/first_warp_web_server/src/main.rs
@@ -1,8 +1,14 @@
-use serde::Serialize;
 use std::fmt::Debug;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
-use warp::{http::StatusCode, reject::Reject, Filter, Rejection, Reply};
+
+use serde::Serialize;
+use warp::{
+    Filter,
+    filters::cors::CorsForbidden,
+    http::Method, http::StatusCode, reject::Reject,
+    Rejection, Reply,
+};
 
 #[derive(Debug, Serialize)]
 struct QuestionId(String);
@@ -56,15 +62,21 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     }
 }
 
-async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(_invalid_id) = r.find::<QuestionId>() {
+async fn return_error(rejection: Rejection) -> Result<impl Reply, Rejection> {
+    println!("Rejection: {:?}", rejection);
+    if let Some(error) = rejection.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
-            "No valid ID presented",
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(_invalid_id) = rejection.find::<InvalidId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else {
         Ok(warp::reply::with_status(
-            "Route not found",
+            "Route not found".to_string(),
             StatusCode::NOT_FOUND,
         ))
     }
@@ -72,6 +84,11 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
 #[tokio::main]
 async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+
     // create a path Filter
     let get_items = warp::get()
         .and(warp::path("questions"))
@@ -79,7 +96,8 @@ async fn main() {
         .and_then(get_questions)
         .recover(return_error);
 
-    let routes = get_items;
+    let routes = get_items.with(cors);
 
+    // To test: curl -X OPTIONS localhost:3030/questions -H "Access-Control-Request-Method: PUT" -H "Access-Control-Request-Headers: content-type" -H "Origin: https://not-origin.io" -verbose
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
