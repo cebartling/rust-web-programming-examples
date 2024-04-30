56115d3e0a4c2ad209c7829b65d1802313e66e57
Christopher Bartling <chris.bartling@gmail.com>
2024-04-29 21:31:30 -0500

    feat: migrating code over

--- chapter_09/rest_server/src/routes/mod.rs
+++ chapter_09/rest_server/src/routes/mod.rs
@@ -1,2 +1,3 @@
 pub(crate) mod answer;
 pub(crate) mod question;
+pub(crate) mod authentication;
--- chapter_09/rest_server/src/types/mod.rs
+++ chapter_09/rest_server/src/types/mod.rs
@@ -1,3 +1,4 @@
 pub(crate) mod answer;
 pub(crate) mod pagination;
 pub(crate) mod question;
+pub(crate) mod account;
