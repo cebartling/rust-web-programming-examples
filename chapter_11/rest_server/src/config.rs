0355e33a2372a83aab67afb3caa4145cb05e1e40
Christopher Bartling <chris.bartling@gmail.com>
2024-05-07 22:04:55 -0500

    feat: wiring up mock server for testing profanity.rs

--- chapter_11/rest_server/src/config.rs
+++ chapter_11/rest_server/src/config.rs
@@ -1,9 +1,9 @@
-use clap::Parser;
-use dotenv;
 use std::env;
 
+use clap::Parser;
+
 /// Q&A web service API
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, PartialEq)]
 #[clap(author, version, about, long_about = None)]
 pub struct Config {
     /// Which errors we want to log (info, warn or error)
@@ -16,7 +16,7 @@ pub struct Config {
     #[clap(long, default_value = "username")]
     pub db_user: String,
     /// Database user
-    #[clap(long)]
+    #[clap(long, default_value = "password")]
     pub db_password: String,
     /// URL for the postgres database
     #[clap(long, default_value = "localhost")]
@@ -31,7 +31,6 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Result<Config, error_handlers::Error> {
-        dotenv::dotenv().ok();
         let config = Config::parse();
 
         if let Err(_) = env::var("BAD_WORDS_API_KEY") {
@@ -69,5 +68,56 @@ impl Config {
                 .map_err(|e| error_handlers::Error::ParseError(e))?,
             db_name,
         })
+    }
+}
+
+#[cfg(test)]
+mod config_unit_tests {
+    use super::*;
+
+    fn set_env() {
+        env::set_var("BAD_WORDS_API_KEY", "yes");
+        env::set_var("PASETO_KEY", "yes");
+        env::set_var("POSTGRES_USER", "user");
+        env::set_var("POSTGRES_PASSWORD", "pass");
+        env::set_var("POSTGRES_HOST", "localhost");
+        env::set_var("POSTGRES_PORT", "5432");
+        env::set_var("POSTGRES_DB", "rustwebdev");
+    }
+
+    fn unset_env() {
+        env::remove_var("BAD_WORDS_API_KEY");
+        env::remove_var("PASETO_KEY");
+        env::remove_var("POSTGRES_USER");
+        env::remove_var("POSTGRES_PASSWORD");
+        env::remove_var("POSTGRES_HOST");
+        env::remove_var("POSTGRES_PORT");
+        env::remove_var("POSTGRES_DB");
+    }
+
+    #[test]
+    fn unset_api_key() {
+        unset_env();
+        let result = std::panic::catch_unwind(|| Config::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn set_api_key() {
+        set_env();
+
+        let expected = Config {
+            log_level: "warn".to_string(),
+            port: 8080,
+            db_user: "user".to_string(),
+            db_password: "pass".to_string(),
+            db_host: "localhost".to_string(),
+            db_port: 5432,
+            db_name: "rustwebdev".to_string(),
+        };
+
+        let config = Config::new().unwrap();
+
+        assert_eq!(config, expected);
     }
 }
--- chapter_11/rest_server/src/profanity.rs
+++ chapter_11/rest_server/src/profanity.rs
@@ -1,12 +1,11 @@
-use reqwest_middleware::ClientBuilder;
-use reqwest_retry::{
-    policies::ExponentialBackoff, RetryTransientMiddleware,
-};
+use std::env;
 use serde::{Deserialize, Serialize};
+use reqwest_middleware::ClientBuilder;
+use reqwest_retry::{RetryTransientMiddleware, policies::ExponentialBackoff};
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct APIResponse {
-    message: String,
+    message: String
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
@@ -27,30 +26,28 @@ struct BadWordsResponse {
     censored_content: String,
 }
 
-pub async fn check_profanity(
-    content: String,
-) -> Result<String, error_handlers::Error> {
-    let retry_policy =
-        ExponentialBackoff::builder().build_with_max_retries(3);
+pub async fn check_profanity(content: String) -> Result<String, error_handlers::Error> {
+    // We are already checking if the ENV VARIABLE is set inside main.rs, so safe to unwrap here
+    let api_key = env::var("BAD_WORDS_API_KEY").expect("BAD WORDS API KEY NOT SET");
+    let api_layer_url = env::var("API_LAYER_URL").expect("APILAYER URL NOT SET");
+
+    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(3);
     let client = ClientBuilder::new(reqwest::Client::new())
         // Trace HTTP requests. See the tracing crate to make use of these traces.
         // Retry failed requests.
         .with(RetryTransientMiddleware::new_with_policy(retry_policy))
         .build();
 
-    let api_key = dotenv::var("BAD_WORDS_API_KEY")
-        .expect("BAD_WORDS_API_KEY must be set");
-    println!("API Key: {}", api_key);
-
     let res = client
-        .post("https://api.apilayer.com/bad_words?censor_character=*")
+        .post(format!(
+            "{}/bad_words?censor_character=*",
+            api_layer_url
+        ))
         .header("apikey", api_key)
         .body(content)
         .send()
         .await
-        .map_err(|e| {
-            error_handlers::Error::MiddlewareReqwestAPIError(e)
-        })?;
+        .map_err(error_handlers::Error::MiddlewareReqwestAPIError)?;
 
     if !res.status().is_success() {
         if res.status().is_client_error() {
@@ -62,17 +59,56 @@ pub async fn check_profanity(
         }
     }
 
-    match res.json::<BadWordsResponse>().await {
+    match res.json::<BadWordsResponse>()
+        .await {
         Ok(res) => Ok(res.censored_content),
         Err(e) => Err(error_handlers::Error::ReqwestAPIError(e)),
     }
 }
 
-async fn transform_error(
-    res: reqwest::Response,
-) -> error_handlers::APILayerError {
+async fn transform_error(res: reqwest::Response) -> error_handlers::APILayerError {
     error_handlers::APILayerError {
         status: res.status().as_u16(),
         message: res.json::<APIResponse>().await.unwrap().message,
+    }
+}
+
+#[cfg(test)]
+mod profanity_tests {
+    use super::{check_profanity, env};
+
+    use mock_server::{MockServer, OneshotHandler};
+
+    #[tokio::test]
+    async fn run() {
+        let handler = run_mock();
+        censor_profane_words().await;
+        no_profane_words().await;
+        let _ = handler.sender.send(1);
+    }
+
+    fn run_mock() -> OneshotHandler {
+        env::set_var("API_LAYER_URL", "http://127.0.0.1:3030");
+        env::set_var("BAD_WORDS_API_KEY", "YES");
+
+        let socket = "127.0.0.1:3030"
+            .to_string()
+            .parse()
+            .expect("Not a valid address");
+        let mock = MockServer::new(socket);
+
+        mock.oneshot()
+    }
+
+    async fn censor_profane_words() {
+        let content = "This is a shitty sentence".to_string();
+        let censored_content = check_profanity(content).await;
+        assert_eq!(censored_content.unwrap(), "this is a ****** sentence");
+    }
+
+    async fn no_profane_words() {
+        let content = "this is a sentence".to_string();
+        let censored_content = check_profanity(content).await;
+        assert_eq!(censored_content.unwrap(), "");
     }
 }
