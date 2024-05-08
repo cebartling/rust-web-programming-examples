86cbd0f3f4264f07f1cb7ae92bc76241bdd8cce2
Christopher Bartling <chris.bartling@gmail.com>
2024-05-07 22:16:30 -0500

    feat: adding unit tests for auth

--- chapter_11/rest_server/src/routes/authentication.rs
+++ chapter_11/rest_server/src/routes/authentication.rs
@@ -2,7 +2,7 @@ use argon2::{self, Config};
 use chrono::prelude::*;
 use rand::Rng;
 use std::{env, future};
-use warp::{http::StatusCode, Filter};
+use warp::{Filter, Rejection, Reply};
 
 use crate::store::Store;
 use crate::types::account::{Account, AccountId, Session};
@@ -10,7 +10,7 @@ use crate::types::account::{Account, AccountId, Session};
 pub async fn register(
     store: Store,
     account: Account,
-) -> Result<impl warp::Reply, warp::Rejection> {
+) -> Result<impl Reply, Rejection> {
     let hashed_password = hash_password(account.password.as_bytes());
 
     let account = Account {
@@ -20,9 +20,7 @@ pub async fn register(
     };
 
     match store.add_account(account).await {
-        Ok(_) => {
-            Ok(warp::reply::with_status("Account added", StatusCode::OK))
-        }
+        Ok(_) => Ok(warp::reply::json(&"Account added".to_string())),
         Err(e) => Err(warp::reject::custom(e)),
     }
 }
@@ -30,7 +28,7 @@ pub async fn register(
 pub async fn login(
     store: Store,
     login: Account,
-) -> Result<impl warp::Reply, warp::Rejection> {
+) -> Result<impl Reply, Rejection> {
     match store.get_account(login.email).await {
         Ok(account) => match verify_password(
             &account.password,
@@ -38,11 +36,9 @@ pub async fn login(
         ) {
             Ok(verified) => {
                 if verified {
-                    let token =
-                        issue_token(account.id.expect("id not found"));
-                    Ok(warp::reply::json(
-                        &serde_json::json!({ "token": token }),
-                    ))
+                    Ok(warp::reply::json(&issue_token(
+                        account.id.expect("id not found"),
+                    )))
                 } else {
                     Err(warp::reject::custom(
                         error_handlers::Error::WrongPassword,
@@ -53,24 +49,10 @@ pub async fn login(
                 error_handlers::Error::ArgonLibraryError(e),
             )),
         },
-        Err(e) => Err(warp::reject::custom(e)),
+        Err(_) => {
+            Err(warp::reject::custom(error_handlers::Error::WrongPassword))
+        }
     }
-}
-
-pub fn verify_token(
-    token: String,
-) -> Result<Session, error_handlers::Error> {
-    let key = env::var("PASETO_KEY").unwrap();
-    let token = paseto::tokens::validate_local_token(
-        &token,
-        None,
-        key.as_bytes(),
-        &paseto::tokens::TimeBackend::Chrono,
-    )
-    .map_err(|_| error_handlers::Error::CannotDecryptToken)?;
-
-    serde_json::from_value::<Session>(token)
-        .map_err(|_| error_handlers::Error::CannotDecryptToken)
 }
 
 fn hash_password(password: &[u8]) -> String {
@@ -86,14 +68,30 @@ fn verify_password(
     argon2::verify_encoded(hash, password)
 }
 
+pub fn verify_token(
+    token: String,
+) -> Result<Session, error_handlers::Error> {
+    let key = env::var("PASETO_KEY").unwrap();
+    let token = paseto::tokens::validate_local_token(
+        &token,
+        None,
+        key.as_bytes(),
+        &paseto::tokens::TimeBackend::Chrono,
+    )
+    .map_err(|_| error_handlers::Error::CannotDecryptToken)?;
+    serde_json::from_value::<Session>(token)
+        .map_err(|_| error_handlers::Error::CannotDecryptToken)
+}
+
 fn issue_token(account_id: AccountId) -> String {
+    let key = env::var("PASETO_KEY").unwrap();
+
     let current_date_time = Utc::now();
     let dt = current_date_time + chrono::Duration::days(1);
-    let key = env::var("PASETO_KEY").unwrap();
+
     paseto::tokens::PasetoBuilder::new()
         .set_encryption_key(&Vec::from(key.as_bytes()))
         .set_expiration(&dt)
-        .set_not_before(&Utc::now())
         .set_claim("account_id", serde_json::json!(account_id))
         .build()
         .expect("Failed to construct paseto token w/ builder!")
@@ -104,9 +102,32 @@ pub fn auth(
     warp::header::<String>("Authorization").and_then(|token: String| {
         let token = match verify_token(token) {
             Ok(t) => t,
-            Err(_) => return future::ready(Err(warp::reject::reject())),
+            Err(_) => {
+                return future::ready(Err(warp::reject::custom(
+                    error_handlers::Error::Unauthorized,
+                )))
+            }
         };
 
         future::ready(Ok(token))
     })
+}
+
+#[cfg(test)]
+mod authentication_tests {
+    use super::{auth, env, issue_token, AccountId};
+
+    #[tokio::test]
+    async fn post_questions_auth() {
+        env::set_var("PASETO_KEY", "RANDOM WORDS WINTER MACINTOSH PC");
+        let token = issue_token(AccountId(3));
+
+        let filter = auth();
+
+        let res = warp::test::request()
+            .header("Authorization", token)
+            .filter(&filter);
+
+        assert_eq!(res.await.unwrap().account_id, AccountId(3));
+    }
 }
